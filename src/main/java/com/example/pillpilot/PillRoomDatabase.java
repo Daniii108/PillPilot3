a74edee77b93d3cd5cd7cee76a1ee9ddf0d07c0b
Daniii108 <127598236+Daniii108@users.noreply.github.com>
2024-12-22 19:21:16 +0100

    commit 2
    ViewModel létrehozása

---
+++ src/main/java/com/example/pillpilot/PillRoomDatabase.java
@@ -0,0 +1,29 @@
+package com.example.pillpilot;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {Pill.class},exportSchema = false, version = 1)
+public abstract class PillRoomDatabase extends RoomDatabase {
+    public abstract PillDao pillDao();
+    private static PillRoomDatabase INSTANCE;
+
+    static PillRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+        synchronized (PillRoomDatabase.class) {
+            if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            PillRoomDatabase.class,
+                            "pill_database").build();
+             }
+        }
+    }
+        return INSTANCE;
+    }
+
+
+}//class
